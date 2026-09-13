8f9da04b83844bc943ec37ca61721117a0f9f6d0
MilaSarac <millasarac@gmail.com>
2026-09-13 04:32:15 +0200

    Kreirana forma za upravljanje kolacima

---
+++ PoslasticarnicaKlijent/src/main/java/rs/ac/bg/fon/ai/forme/kolac/FormaKolac.java
@@ -0,0 +1,201 @@
+package rs.ac.bg.fon.ai.forme.kolac;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.border.TitledBorder;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.GroupLayout;
+import javax.swing.LayoutStyle.ComponentPlacement;
+
+public class FormaKolac extends JFrame {
+
+    private static final long serialVersionUID = 1L;
+
+    private JPanel pnlKolaci;
+
+    private JLabel lblPretraga;
+    private JTextField txtPretraga;
+    private JButton btnPretrazi;
+
+    private JLabel lblNaziv;
+    private JTextField txtNaziv;
+
+    private JLabel lblCena;
+    private JTextField txtCena;
+
+    private JLabel lblOpis;
+    private JTextField txtOpis;
+
+    private JButton btnDodaj;
+    private JButton btnIzmeni;
+    private JButton btnObrisi;
+
+    private JScrollPane scrollPane;
+    private JTable tblKolaci;
+
+    public FormaKolac() {
+        initialize();
+        setLocationRelativeTo(null);
+    }
+
+    private void initialize() {
+
+        setTitle("Kolaci");
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        setBounds(100, 100, 746, 791);
+
+        pnlKolaci = new JPanel();
+        pnlKolaci.setBorder(new TitledBorder(null, "Kolaci", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+
+        setContentPane(pnlKolaci);
+
+        lblPretraga = new JLabel("Pretraga po nazivu:");
+
+        txtPretraga = new JTextField();
+        txtPretraga.setColumns(10);
+
+        btnPretrazi = new JButton("Pretraži");
+
+        lblNaziv = new JLabel("Naziv:");
+
+        txtNaziv = new JTextField();
+        txtNaziv.setColumns(10);
+
+        lblCena = new JLabel("Cena:");
+
+        txtCena = new JTextField();
+        txtCena.setColumns(10);
+
+        lblOpis = new JLabel("Opis:");
+
+        txtOpis = new JTextField();
+        txtOpis.setColumns(10);
+
+        btnDodaj = new JButton("Dodaj");
+
+        btnDodaj.addActionListener(e -> {
+            btnDodajActionPerformed();
+        });
+
+        btnIzmeni = new JButton("Izmeni");
+
+        btnIzmeni.addActionListener(e -> {
+            btnIzmeniActionPerformed();
+        });
+
+        btnObrisi = new JButton("Obriši");
+
+        btnObrisi.addActionListener(e -> {
+            btnObrisiActionPerformed();
+        });
+
+        btnPretrazi.addActionListener(e -> {
+            btnPretraziActionPerformed();
+        });
+
+        scrollPane = new JScrollPane();
+
+        tblKolaci = new JTable();
+
+        tblKolaci.setModel(new javax.swing.table.DefaultTableModel(
+                new Object[][] {
+                },
+                new String[] {
+                        "Naziv", "Cena", "Opis"
+                }
+        ));
+
+        scrollPane.setViewportView(tblKolaci);
+
+        javax.swing.GroupLayout layout =
+                new javax.swing.GroupLayout(pnlKolaci);
+        layout.setHorizontalGroup(
+        	layout.createParallelGroup(Alignment.TRAILING)
+        		.addGroup(Alignment.LEADING, layout.createSequentialGroup()
+        			.addGap(30)
+        			.addGroup(layout.createParallelGroup(Alignment.LEADING)
+        				.addGroup(layout.createParallelGroup(Alignment.TRAILING, false)
+        					.addGroup(Alignment.LEADING, layout.createSequentialGroup()
+        						.addGroup(layout.createParallelGroup(Alignment.LEADING)
+        							.addComponent(lblNaziv)
+        							.addComponent(lblCena)
+        							.addComponent(lblOpis))
+        						.addGap(35)
+        						.addGroup(layout.createParallelGroup(Alignment.LEADING, false)
+        							.addComponent(txtNaziv)
+        							.addComponent(txtOpis)
+        							.addComponent(txtCena, GroupLayout.DEFAULT_SIZE, 532, Short.MAX_VALUE)
+        							.addComponent(btnDodaj, Alignment.TRAILING, GroupLayout.PREFERRED_SIZE, 149, GroupLayout.PREFERRED_SIZE)))
+        					.addGroup(Alignment.LEADING, layout.createSequentialGroup()
+        						.addComponent(lblPretraga)
+        						.addGap(18)
+        						.addComponent(txtPretraga, GroupLayout.PREFERRED_SIZE, 337, GroupLayout.PREFERRED_SIZE)
+        						.addPreferredGap(ComponentPlacement.UNRELATED)
+        						.addComponent(btnPretrazi, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
+        				.addGroup(layout.createSequentialGroup()
+        					.addGap(8)
+        					.addComponent(scrollPane, GroupLayout.PREFERRED_SIZE, 436, GroupLayout.PREFERRED_SIZE)
+        					.addGap(18)
+        					.addGroup(layout.createParallelGroup(Alignment.LEADING, false)
+        						.addComponent(btnObrisi, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        						.addComponent(btnIzmeni, GroupLayout.DEFAULT_SIZE, 119, Short.MAX_VALUE))))
+        			.addGap(300))
+        );
+        layout.setVerticalGroup(
+        	layout.createParallelGroup(Alignment.LEADING)
+        		.addGroup(layout.createSequentialGroup()
+        			.addGap(25)
+        			.addGroup(layout.createParallelGroup(Alignment.BASELINE)
+        				.addComponent(lblPretraga)
+        				.addComponent(txtPretraga, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+        				.addComponent(btnPretrazi))
+        			.addGap(25)
+        			.addGroup(layout.createParallelGroup(Alignment.BASELINE)
+        				.addComponent(lblNaziv)
+        				.addComponent(txtNaziv, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+        			.addGap(15)
+        			.addGroup(layout.createParallelGroup(Alignment.BASELINE)
+        				.addComponent(lblCena)
+        				.addComponent(txtCena, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+        			.addGap(15)
+        			.addGroup(layout.createParallelGroup(Alignment.BASELINE)
+        				.addComponent(lblOpis)
+        				.addComponent(txtOpis, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+        			.addGroup(layout.createParallelGroup(Alignment.LEADING)
+        				.addGroup(layout.createSequentialGroup()
+        					.addPreferredGap(ComponentPlacement.UNRELATED)
+        					.addComponent(btnDodaj)
+        					.addGap(49)
+        					.addComponent(btnIzmeni)
+        					.addGap(30)
+        					.addComponent(btnObrisi))
+        				.addGroup(layout.createSequentialGroup()
+        					.addGap(45)
+        					.addComponent(scrollPane, GroupLayout.PREFERRED_SIZE, 250, GroupLayout.PREFERRED_SIZE)))
+        			.addContainerGap(280, Short.MAX_VALUE))
+        );
+
+        pnlKolaci.setLayout(layout);
+    }
+
+    private void btnDodajActionPerformed() {
+
+    }
+
+    private void btnIzmeniActionPerformed() {
+
+    }
+
+    private void btnObrisiActionPerformed() {
+
+    }
+
+    private void btnPretraziActionPerformed() {
+
+    }
+}
